622daff0f82f61fda3fd919fc0847632d6ac51a4
Ademar Castro <ademar.castro.fh@gmail.com>
2023-07-26 14:59:34 -0400

    Update: Terceiro módulo: Modal Components (Admin) concluído.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,22 @@
-import { UserButton } from "@clerk/nextjs";
+"use client";
+
+import { useEffect } from "react";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
 
 const SetupPage = () => {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, []);
+
   return (
     <div className='p-4'>
-      <UserButton afterSignOutUrl="/" />
-      Está é uma rota protegida!
+      Root Page
     </div>
   );
 }
